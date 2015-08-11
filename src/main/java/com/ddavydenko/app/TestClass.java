00f59cb4ec180e63aabd4f63e155149aef4ba6ff
Davydenko Dmitriy <ddavydenko@plslogistics.com>
2015-08-11 16:52:32 +0300

    Just because

--- src/main/java/com/ddavydenko/app/TestClass.java
+++ src/main/java/com/ddavydenko/app/TestClass.java
@@ -1,5 +1,6 @@
 package com.ddavydenko.app;
 
+<<<<<<< HEAD
 import javax.swing.JOptionPane;
 
 public class TestClass {
@@ -16,12 +17,36 @@ public class TestClass {
     
     public static boolean isFirstBigger(int a, int b)
     {
+=======
+import java.util.Arrays;
+
+import javax.swing.JOptionPane;
+
+import org.junit.Ignore;
+
+public class TestClass {
+
+	public static int getThree() {
+		return 3;
+	}
+	
+    public static String concatStrings(String a, String b) {
+        return a+b;  
+    }
+    
+    public static int addNumbers(int a, int b) {
+        return a+b;  
+    }
+    
+    public static boolean isFirstBigger(int a, int b) {
+>>>>>>> Just because
         if(a<=100 && b<=100)
         {
             return a>b;
         }
        return false;
     }
+<<<<<<< HEAD
     public static int addThree(int number)
     {
         if(number>=0)
@@ -30,5 +55,30 @@ public class TestClass {
         }
         else JOptionPane.showMessageDialog(null, "The number should not be negative!");
         return -1;
+=======
+    
+    public static int doublesTheValue(int number) {
+    	
+        return number+=number;
+        
+    }
+    
+    public static int[] reverseArray(int[] arr) {
+    	int temp = 0;
+    	if(arr.length!=0 || arr.length!=1) {
+    		for(int i=0; i<arr.length/2; i++) {
+        		temp = arr[i];
+        		arr[i]=arr[arr.length-i-1];
+        		arr[arr.length-i-1] = temp;
+        	}
+        	return arr;
+    	}
+    	else return arr;
+    }
+    
+    @Ignore
+    public static void ignoreTest() {
+    	System.out.println("Nevermind of what is written here.");
+>>>>>>> Just because
     }
 }

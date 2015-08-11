05b82a6bbc6cc5cc827238c30b49e776b06e0cf2
Davydenko Dmitriy <ddavydenko@plslogistics.com>
2015-08-11 16:41:36 +0300

    Changed something

--- src/test/java/com/ddavydenko/app/AppTest.java
+++ src/test/java/com/ddavydenko/app/AppTest.java
@@ -1,39 +1,105 @@
 package com.ddavydenko.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Test
+    public void additionStringsTest() {
+        TestClass test = new TestClass();
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+        String s= test.concatStrings("Hello, ", "there.");
+        String expected = "Hello, there.";
+        assertEquals("Method should concatinate 2 strings.", expected, s);
+    };
+    @Test 
+    public void additionNumbersTest() {
+    	TestClass test = new TestClass();
+    	
+    	int res = test.addNumbers(1, 100500);
+    	int expected = 100501;
+    	assertEquals("Method should sum up 2 numbers.", expected, res);
+    }
+    @Test
+    public void getThreeTest() {
+    	TestClass test = new TestClass();
+    	assertEquals("Method should return number 3.", 3, test.getThree());
+    }
+    @Test
+    public void doublesTheValueTest() {
+    	TestClass test = new TestClass();
+    	int testValue = 20;
+    	
+    	assertEquals("Method should double the value.", 40, test.doublesTheValue(testValue));
+    }
+    
+    //Checks if the first value is greater than 2-nd.
+    @Test
+    public void isFirstBiggerTest() {
+    	TestClass test = new TestClass();
+    	
+    	assertTrue(test.isFirstBigger(26, 25));
+    	assertFalse(test.isFirstBigger(25, 25));
+    	assertFalse(test.isFirstBigger(101, 99));
+    	assertFalse(test.isFirstBigger(102, 101));
+    	assertTrue(test.isFirstBigger(-10, -20));
+    }
+    
+    //Checks if method reverses the given array.
+    @Test
+    public void reverseArrayTest() {
+    	TestClass test = new TestClass();
+    	
+    	int[] testArray = {1,2,3,4,5,6,7,8};
+    	int[] testArray2 = {1};
+    	int[] testArray3 = {};
+    	int[] expectedArray = {8,7,6,5,4,3,2,1};
+    	int[] expectedArray2 = {1};
+    	int[] expectedArray3 = {};
+    	
+    	int[] actualArray = test.reverseArray(testArray);
+    	
+    	assertArrayEquals(expectedArray, actualArray);
+    	assertArrayEquals(testArray2,expectedArray2);
+    	assertArrayEquals(testArray3,expectedArray3);
+    }
+    
+    //Checks if method throws NullPointerException and if the test object is not null
+    @Test (expected = NullPointerException.class)
+    public void reverseArrayNullPointerTest() {
+    	TestClass test = new TestClass();
+    	
+    	int[] testArray = null;
+    	
+    	test.reverseArray(testArray);
+    	
+    	assertNotNull(test);
+    }
+    
+    //Just playing tiwh asserts of JUnit
+    @Test
+    public void objectReferenceTest() {
+    	String s1 = "BOLD";
+    	String s2 = "BOLD";
+    	String s3 = "Bold";
+    	TestClass test = new TestClass();
+    	TestClass test2 = new TestClass();
+    	
+    	
+    	assertSame(s1,s2);
+    	assertNotSame(s2,s3);
+    	assertEquals(s1,s3.toUpperCase());
+    	assertNotSame(s1,s3.toUpperCase());
+    	assertNotSame(test, test2);
+    	
+    	test = test2;
+    	
+    	assertSame(test,test2);
     }
     
     

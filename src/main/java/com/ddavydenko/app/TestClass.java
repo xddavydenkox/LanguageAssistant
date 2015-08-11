800a0b3e30c8c1f4c34120984d25d15d18e5956a
Davydenko Dmitriy <ddavydenko@plslogistics.com>
2015-08-11 17:47:36 +0300

    Return project to working condition

--- src/main/java/com/ddavydenko/app/TestClass.java
+++ src/main/java/com/ddavydenko/app/TestClass.java
@@ -36,16 +36,16 @@ public class TestClass {
         return number+=number;
     }
 
-//    public static int[] reverseArray(int[] arr) {
-//        int temp = 0;
-//    	if(arr.length!=0 || arr.length!=1) {
-//    		for(int i=0; i<arr.length/2; i++) {
-//        		temp = arr[i];
-//        		arr[i]=arr[arr.length-i-1];
-//        		arr[arr.length-i-1] = temp;
-//        	}
-//        	return arr;
-//    	}
-//    	else return arr;
-//    }
+    public static int[] reverseArray(int[] arr) {
+        int temp = 0;
+    	if(arr.length!=0 || arr.length!=1) {
+    		for(int i=0; i<arr.length/2; i++) {
+        		temp = arr[i];
+        		arr[i]=arr[arr.length-i-1];
+        		arr[arr.length-i-1] = temp;
+        	}
+        	return arr;
+    	}
+    	else return arr;
+    }
 }
